28dc5537bec4ae5259b0d4e503b31168e036ce61
Endijs Bertāns <s22bertendi@vea.lv>
2024-02-28 09:47:41 +0200

    datastr construct

--- src/datastr/MyArrayList.java
+++ src/datastr/MyArrayList.java
@@ -2,9 +2,31 @@ package datastr;
 
 public class MyArrayList {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private int[] list;
+	private final int LIST_SIZE = 10;
+	private int size = LIST_SIZE;
+	private int counter = 0;
+	
+	//TODO
+	//constructors isempty, isfull, howmanyelements
+	public MyArrayList() {
+		list = new int[size];
 	}
-
+	
+	public MyArrayList(int inputSize) {
+		if(inputSize > 0) size = inputSize;
+		list = new int[size];
+	}
+	
+	public boolean isEmpty() {
+		return counter == 0;
+	}
+	
+	public boolean isFull() {
+		return counter == size;
+	}
+	
+	public int howManyElements() {
+		return counter;
+	}
 }
